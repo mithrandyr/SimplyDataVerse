1d792be8763419b0a491472c43c86b45780f7200
mithrandyr <mithrandyr@users.noreply.github.com>
2025-06-19 16:49:56 -0400

    notes to self

--- SimplyDataVerse/PublicFunctions/Get-DataVerseRows.ps1
+++ SimplyDataVerse/PublicFunctions/Get-DataVerseRows.ps1
@@ -28,6 +28,15 @@ function Get-DataVerseRows {
         EndPoint = $ep
         AddHeaders = $addHdrs
     }
+    <#this needs to handle lookup columns, using the $expand option.
+        on list of columns, for every lookup column, append _value (this will retrieve the guid)
+        if the option -Expand is used, then expand the columns using $expand uri option
+
+        probably need to create a function to generate a psobject with the type name, then this can be
+        handled recursively.
+
+    #>
+
     Invoke-DataVerse @request |
         Select-Object -ExpandProperty value |
         ForEach-Object {
